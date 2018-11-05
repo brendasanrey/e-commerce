fdd81e52a6c6146cb0c7c1b15ecbf6a81bce9305
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-11-04 18:41:33 -0600

    Agregando archivos base para template

---
+++ mainView/views/components/header.php
@@ -0,0 +1,11 @@
+<div class="container-fluid" id="top">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-9 col-md-9 col-sm-8 col-12">
+      <h1>HOLA</h1>
+      </div>
+
+    </div>
+  </div>
+
+</div>
