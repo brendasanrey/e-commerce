5d2ff90d567cd618ee6f7a561f0cb921e3088f12
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-11-04 19:48:04 -0600

    Agregando maquetación header

--- mainView/views/components/header.php
+++ mainView/views/components/header.php
@@ -1,11 +1,144 @@
 <div class="container-fluid" id="top">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-9 col-md-9 col-sm-8 col-12">
-      <h1>HOLA</h1>
-      </div>
+  <div class="row">
 
+  <!-- NOTE Redes sociales -->
+    <div class="col-lg-9 col-md-9 col-sm-8 col-12 social">
+      <ul>
+        <li>
+          <a href="#"><i class="fab fa-facebook-square text-white social-networks"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-youtube text-white social-networks"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-twitter text-white social-networks"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-instagram text-white social-networks"></i></a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- NOTE Registro -->
+    <div class="col-lg-3 col-md-3 col-sm-4 col-12 register">
+      <ul>
+        <li>
+          <a href="#modalSignIn" data-toggle="modal">Ingresar</a>
+        </li>
+        <li>|</li>
+        <li>
+            <a href="#modalSignUp" data-toggle="modal">Registro</a>
+        </li>
+      </ul>
     </div>
   </div>
-
 </div>
+
+<header class="container-fluid">
+  <div class="row" id="header">
+  <!-- NOTE logo -->
+    <div class="col-lg-3 col-md-3 col-sm-2 col-12" id="logo">
+      <a href="#">
+        <img src="../adminView/views/resources/assets/images/mi-logo.png" alt="img-fluid">
+      </a>
+    </div>
+
+    <!-- NOTE Boton Categorias y buscador -->
+    <div class="col-lg-6 col-md-6 col-sm-8 col-12">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-12 color-bg" id="btnCategories">
+          <p>Categorías
+            <span class="float-right"><i class="fas fa-bars"></i></span>
+          </p>
+        </div>
+
+        <div class="input-group col-lg-8 col-md-8 col-sm-8 col-12" id="search">
+          <input type="text" placeholder="Buscar" class="form-control">
+          <div class="input-group-append">
+            <button class="btn btn-default color-bg" type="submit"><i class="fas fa-search"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- NOTE Carrito de compras -->
+    <div class="col-lg-3 col-md-3 col-sm-2 col-12" id="shoppingCar">
+      <a href="#">
+        <button class="btn btn-default float-left color-bg">
+          <i class="fas fa-shopping-cart"></i>
+        </button>
+      </a>
+      <p>
+        Pedido <span class="amount"></span> <br> MXN $ <span class="total"></span>
+      </p>
+    </div>
+
+    <!-- NOTE Categorías -->
+    <div class="col-12 color-bg" id="catogories">
+      <div class="row">
+
+        <div class="col-lg-2 col-md-3 col-sm-4 col-12">
+          <h4><a href="#" class="pixel-categories">Mujeres</a></h4>
+          <hr>
+          <ul>
+            <li><a href="#" class="pixel-sub-categories">Vestidos</a></li>
+            <li><a href="#" class="pixel-sub-categories">Blusas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Pantalones</a></li>
+            <li><a href="#" class="pixel-sub-categories">Sudaderas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Accesorios</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 col-sm-4 col-12">
+          <h4><a href="#" class="pixel-categories">Hombres</a></h4>
+          <hr>
+          <ul>
+            <li><a href="#" class="pixel-sub-categories">Trajes</a></li>
+            <li><a href="#" class="pixel-sub-categories">Camisas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Pantalones</a></li>
+            <li><a href="#" class="pixel-sub-categories">Sudaderas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Accesorios</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 col-sm-4 col-12">
+          <h4><a href="#" class="pixel-categories">Niñas</a></h4>
+          <hr>
+          <ul>
+            <li><a href="#" class="pixel-sub-categories">Vestidos</a></li>
+            <li><a href="#" class="pixel-sub-categories">Blusas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Pantalones</a></li>
+            <li><a href="#" class="pixel-sub-categories">Sudaderas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Accesorios</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 col-sm-4 col-12">
+          <h4><a href="#" class="pixel-categories">Niños</a></h4>
+          <hr>
+          <ul>
+            <li><a href="#" class="pixel-sub-categories">Trajes</a></li>
+            <li><a href="#" class="pixel-sub-categories">Camisas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Pantalones</a></li>
+            <li><a href="#" class="pixel-sub-categories">Sudaderas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Accesorios</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 col-sm-4 col-12">
+          <h4><a href="#" class="pixel-categories">Bebés</a></h4>
+          <hr>
+          <ul>
+            <li><a href="#" class="pixel-sub-categories">Conjuntos</a></li>
+            <li><a href="#" class="pixel-sub-categories">Camisas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Pantalones</a></li>
+            <li><a href="#" class="pixel-sub-categories">Sudaderas</a></li>
+            <li><a href="#" class="pixel-sub-categories">Accesorios</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</header>
